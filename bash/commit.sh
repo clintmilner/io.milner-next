4d06f4496a864e78ed079568d7130d615d2bfed7
Clint Milner <clinton.milner@rebasoft.net>
2019-05-31 16:11:31 +0100

    testing the sleep command

--- bash/commit.sh
+++ bash/commit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo "Commit has been committed and is being pushed to repo - let's get to work!"
+echo "logging into Raspberry Pi..."
 
-echo "TODO: push code to Raspberry Pi"
-
-pwd
-ssh pi@10.100.1.16 'cd ~/dev/io.milner-next; git pull; npm run start'
+ssh -t pi@10.100.1.16 'sleep 10s; cd ~/dev/io.milner-next; git pull; npm run start'
 EOF
