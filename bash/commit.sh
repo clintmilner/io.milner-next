05030da9beb7ddf1cbd406dd5d6b7b50c8f19141
Clint Milner <clinton.milner@rebasoft.net>
2019-05-31 16:06:00 +0100

    testing remote pull script

--- bash/commit.sh
+++ bash/commit.sh
@@ -4,3 +4,5 @@ echo "Commit has been committed and is being pushed to repo - let's get to work!
 echo "TODO: push code to Raspberry Pi"
 
 pwd
+ssh pi@10.100.1.16 'cd ~/dev/io.milner-next; git pull; npm run start'
+EOF
