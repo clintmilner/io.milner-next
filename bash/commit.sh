42283080dc47985b7689317ad5faad5e1f93fa3d
Clint Milner <clinton.milner@rebasoft.net>
2019-05-31 16:15:45 +0100

    this isn't working like I thought it would...

--- bash/commit.sh
+++ bash/commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "logging into Raspberry Pi..."
 
-ssh -t pi@10.100.1.16 'sleep 10s; cd ~/dev/io.milner-next; git pull; npm run start'
+#ssh -t pi@10.100.1.16 'sleep 10s; cd ~/dev/io.milner-next; git pull; npm run start'
 EOF
